340d0aa87aadd223598dd734bbb400ccbee48cd3
Abderrahim <amalou.abdr@gmail.com>
2022-12-12 13:38:57 -0500

    desactivating getting config and config ocnos IP

--- network/ocnos_service/ocnos_service.py
+++ network/ocnos_service/ocnos_service.py
@@ -38,8 +38,12 @@ class OcnosService:
         logging.debug("data collected from {} is : {} ".format(device,query_dictionary))
         return query_dictionary
     
-    def ctl2sonic_parse(self, config):
-        return config
+    def ctl2ocnos_parse(self, config):
+        interface = config['INTERFACE'].split('|')[0]
+        ip = config['INTERFACE'].split('|')[1]
+        commands = ['enable', 'configure terminal', 'interface '+interface, 'ip address '+ip, 'exit', 'commit', 'end', 'disable']
+
+        return commands
 
     def backup_device(self):
         cmd = "copy running-config file:/running-config-bkp"
@@ -49,38 +53,12 @@ class OcnosService:
 
 
     def config_device(self, device, config):
-        config = self.ctl2sonic_parse(config)
+        commands = self.ctl2ocnos_parse(config)
         #open sftp session to get and put files from and to the remote device
         ftp_client=self.client.open_sftp()
-        with open('config.json', 'w') as outfile:
-            json.dump(config, outfile,indent=4)
-        outfile.close()
-        ftp_client.put('config.json','config.json')
-        #create back-up config
-        cmd = "sudo cp /etc/sonic/config_db.json /etc/sonic/config_db.json.bk"
-        self.exe_cmd(cmd)
-        status = 0
-        cmd = "sonic-cfggen -j config.json"
-        if self.exe_cmd(cmd)!=0:
-            status =1
-            logging.error("Error in json format ")
-        cmd = "sudo config load config.json -y"
-        if self.exe_cmd(cmd)!=0:
-            status = 1
-            logging.error("Error in loading config from file")
-        cmd = "sudo config save -y"
-        if self.exe_cmd(cmd)!=0:
-            status = 1
-            logging.error("Error in saving config")
-        if status == 0:
-            logging.info("{} Configured successfully".format(device))
-            self.client.close()
-            return 0
-        else:
-            logging.warning("{} Configuration not successful ... backup of the old configuration".format(device))
-            self.backup_device()
-            self.client.close()
-            return -1
         
+        #execute configuration commands
+        for cmd in commands:
+            self.exe_cmd(cmd)
     
     
